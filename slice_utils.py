d1343b1866a8a5b2ccef94a44b4708960f02e92d
Andrei Savici <saviciat@ornl.gov>
2022-01-23 22:40:26 -0500

    Fix minor bug

--- slice_utils.py
+++ slice_utils.py
@@ -43,6 +43,7 @@ def make_slice(data_set,slice_description, solid_angle_ws=None, ASCII_slice_fold
 
     # transform to chi''
     is_chi = slice_description.get('ConvertToChi',False)
+
     temperature=0.
     if is_chi:
         try:
@@ -50,8 +51,8 @@ def make_slice(data_set,slice_description, solid_angle_ws=None, ASCII_slice_fold
         except:
             raise ValueError("For calculating chi'' one needs to set the temperature in the dataset definition. See example.")
         if not mtd.doesExist(mde_name+'_chi'):
-            ApplyDetailedBalanceMD(InputWorkspace=mde_name, Temperature=data_set['SampleLogVariables']['Temperature'], OutputWorkspace=mde_name+'_chi')
-            mdnorm_parameters['InputWorkspace']=mde_name+'_chi'
+            ApplyDetailedBalanceMD(InputWorkspace=mde_name, Temperature=str(data_set['SampleLogVariables']['Temperature']), OutputWorkspace=mde_name+'_chi')
+        mdnorm_parameters['InputWorkspace']=mde_name+'_chi'
     bg_type=None
     bg_mde_name=data_set.get("BackgroundMdeName")
     if bg_mde_name is not None:
@@ -66,8 +67,8 @@ def make_slice(data_set,slice_description, solid_angle_ws=None, ASCII_slice_fold
                 raise ValueError('BG MDE not found: please run the reduction on BG runs to make the BG MDE '+bg_mde_name)
         if is_chi:
             if not mtd.doesExist(bg_mde_name+'_chi'):
-                ApplyDetailedBalanceMD(InputWorkspace=bg_mde_name, Temperature=data_set['SampleLogVariables']['Temperature'], OutputWorkspace=bg_mde_name+'_chi')
-                bg_mde_name+='_chi'
+                ApplyDetailedBalanceMD(InputWorkspace=bg_mde_name, Temperature=str(data_set['SampleLogVariables']['Temperature']), OutputWorkspace=bg_mde_name+'_chi')
+            bg_mde_name+='_chi'
         if mtd[bg_mde_name].getSpecialCoordinateSystem()==mantid.kernel.SpecialCoordinateSystem.QLab:
             mdnorm_parameters['BackgroundWorkspace'] = bg_mde_name
             mdnorm_parameters['OutputBackgroundDataWorkspace'] = '_bkg_data'
