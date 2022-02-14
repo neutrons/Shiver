9482447a84ccc0aef16df2afea85edfd4e0ee8ec
Andrei Savici <saviciat@ornl.gov>
2022-02-14 16:37:09 -0500

    Add hyspec example

--- oncat_example.py
+++ oncat_example.py
@@ -10,7 +10,7 @@ except (NameError, pyoncat.LoginRequiredError, pyoncat.InvalidRefreshTokenError)
 # generate file and print the keys
 print(
     write_json_from_oncat(
-        filename="/home/3y9/Desktop/deleteme.txt",
+        filename="/SNS/users/3y9/Desktop/deleteme.txt",
         login=ocl,
         ipts_number="27023",
         instrument="ARCS",
@@ -20,12 +20,37 @@ print(
 
 # delete one particular run from a particular dataset
 write_json_from_oncat(
-    filename="/home/3y9/Desktop/deleteme.txt",
+    filename="/SNS/users/3y9/Desktop/deleteme.txt",
     login=ocl,
     ipts_number="27023",
     instrument="ARCS",
-    dataset_name="U2Zn17_30meV_res_5K",
+    dataset_name="need_correct_label_30meV_res_5K",
     exclude_runs=[222669],
     group_by_angle=True,
     append=True,
 )
+
+# add entry for some runs that were not labeled as part of a dataset
+write_json_from_oncat(
+    filename="/SNS/users/3y9/Desktop/deleteme.txt",
+    login=ocl,
+    ipts_number="27023",
+    instrument="ARCS",
+    dataset_name="NewDataset",
+    include_runs=range(222204, 222210),
+    group_by_angle=True,
+    angle_bin=10.,
+    append=True,
+)
+
+# generate file for HYSPEC where datasets were labeled by the notes
+print(
+    write_json_from_oncat(
+        filename="/SNS/users/3y9/Desktop/deleteme1.txt",
+        login=ocl,
+        ipts_number="27585",
+        instrument="HYS",
+        use_notes=True,
+    )
+)
+
