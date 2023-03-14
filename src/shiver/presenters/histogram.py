6a2d8b546f8bc96f5031d40e3da023528e713ad0
Ross Whitfield <whitfieldre@ornl.gov>
2023-03-14 16:53:46 -0400

    Add loading to load buttons

--- src/shiver/presenters/histogram.py
+++ src/shiver/presenters/histogram.py
@@ -8,6 +8,13 @@ class HistogramPresenter:
         self._view = view
         self._model = model
 
+        self.view.buttons.connect_load_file(self.load_file)
+        self.model.connect_error_message(self.error_message)
+
+    def load_file(self, file_type, filename):
+        """Call model to load the filename from the UI file dialog"""
+        self.model.load(filename, file_type)
+
     @property
     def view(self):
         """Return the view for this presenter"""
@@ -17,3 +24,7 @@ class HistogramPresenter:
     def model(self):
         """Return the model for this presenter"""
         return self._model
+
+    def error_message(self, msg):
+        """Pass error message to the view"""
+        self.view.show_error_message(msg)
---
+++ src/shiver/views/loading_buttons.py
@@ -0,0 +1,50 @@
+"""PyQt widget for the histogram tab"""
+from qtpy.QtWidgets import (
+    QWidget,
+    QVBoxLayout,
+    QPushButton,
+    QFileDialog,
+)
+
+
+class LoadingButtons(QWidget):
+    """Buttons for Loading"""
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.load_dataset = QPushButton("Load dataset")
+        self.gen_dataset = QPushButton("Generate dataset")
+        self.load_mde = QPushButton("Load MDE")
+        self.load_norm = QPushButton("Load normalization")
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.load_dataset)
+        layout.addWidget(self.gen_dataset)
+        layout.addWidget(self.load_mde)
+        layout.addWidget(self.load_norm)
+        layout.addStretch()
+        self.setLayout(layout)
+
+        self.load_mde.clicked.connect(self._on_load_mde_click)
+        self.load_norm.clicked.connect(self._on_load_norm_click)
+
+        self.file_load_callback = None
+
+    def _on_load_mde_click(self):
+        filename, _ = QFileDialog.getOpenFileName(
+            self, "Select one or more files to open", "", "NeXus file (*.nxs);;All files (*)"
+        )
+        if filename and self.file_load_callback:
+            self.file_load_callback("mde", filename)
+
+    def _on_load_norm_click(self):
+        filename, _ = QFileDialog.getOpenFileName(
+            self, "Select one or more files to open", "", "NeXus file (*.nxs *.nx5);;All files (*)"
+        )
+        if filename and self.file_load_callback:
+            self.file_load_callback("norm", filename)
+
+    def connect_load_file(self, callback):
+        """connect a function to the selection of a filename"""
+        self.file_load_callback = callback
