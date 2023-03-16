261eaf2b980591174b09daec083709fd6cba7359
Chen Zhang <zhangc@ornl.gov>
2023-03-16 15:00:25 -0400

    add histogram test

---
+++ tests/views/test_histogram.py
@@ -0,0 +1,20 @@
+"""UI test for the histogram tab"""
+import os
+from shiver import Shiver
+from mantid.simpleapi import LoadMD
+
+def test_histogram(qtbot):
+    """Test the mde and norm lists"""
+    shiver = Shiver()
+    qtbot.addWidget(shiver)
+    shiver.show()
+
+    LoadMD(
+        Filename=os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "../data/mde/merged_mde_MnO_25meV_5K_unpol_178921-178926.nxs"
+        ),
+        OutputWorkspace="data",
+    )
+
+    # check that the workspace is in the norm list
+    assert shiver.main_window.histogram.input_workspaces.norm_workspaces.count() == 1
