4923f7f96569a9d801e3826882bcc16c93a7dfbe
Ross Whitfield <whitfieldre@ornl.gov>
2023-03-29 16:12:22 -0400

    Add rename/delete menu to norm table

--- src/shiver/views/histogram.py
+++ src/shiver/views/histogram.py
@@ -63,7 +63,9 @@ class Histogram(QWidget):
     def connect_delete_workspace(self, callback):
         """connect a function to the selection of a filename"""
         self.input_workspaces.mde_workspaces.delete_workspace_callback = callback
+        self.input_workspaces.norm_workspaces.delete_workspace_callback = callback
 
     def connect_rename_workspace(self, callback):
         """connect a function to the selection of a filename"""
         self.input_workspaces.mde_workspaces.rename_workspace_callback = callback
+        self.input_workspaces.norm_workspaces.rename_workspace_callback = callback
