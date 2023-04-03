1b44ad60fc28c44b4c31a2c20f487593ed6341d3
Ross Whitfield <whitfieldre@ornl.gov>
2023-04-03 12:06:00 -0400

    Add save data button merge conflicts v2

--- src/shiver/views/workspace_tables.py
+++ src/shiver/views/workspace_tables.py
@@ -471,6 +471,7 @@ class MDHList(ADSList):
         self.delete_workspace_callback = None
         self.setContextMenuPolicy(Qt.CustomContextMenu)
         self.customContextMenuRequested.connect(self.contextMenu)
+        self.save_callback = None
         self.save_script_callback = None
 
     def contextMenu(self, pos):  # pylint: disable=invalid-name
@@ -491,7 +492,9 @@ class MDHList(ADSList):
         script.triggered.connect(partial(self.save_script, selected_ws))
         menu.addAction(script)
 
-        menu.addAction("Save Data")
+        save = QAction("Save Data")
+        save.triggered.connect(partial(self.save_ws, selected_ws))
+        menu.addAction(save)
 
         menu.addSeparator()
 
@@ -510,6 +513,14 @@ class MDHList(ADSList):
         )
         if filename and self.save_script_callback:
             self.save_script_callback(name, filename)  # pylint: disable=not-callable
+
+    def save_ws(self, name):
+        """method to handle the saving of script"""
+        filename, _ = QFileDialog.getSaveFileName(
+            self, "Select location to save workspace", "", "Python script (*.nxs);;All files (*)"
+        )
+        if filename and self.save_callback:
+            self.save_callback(name, filename)  # pylint: disable=not-callable
 
     def delete_ws(self, name):
         """methed to delete the currently selected workspace"""
