ad34afb5e6bcbe45b1271dfa1a7db51f8eb10352
Ross Whitfield <whitfieldre@ornl.gov>
2023-04-12 17:01:26 -0400

    Add Raw Data selection widget

--- src/shiver/views/generate.py
+++ src/shiver/views/generate.py
@@ -6,6 +6,7 @@ from qtpy.QtWidgets import (
     QGroupBox,
     QPushButton,
 )
+from .data import RawData
 
 
 class Generate(QWidget):
@@ -15,20 +16,19 @@ class Generate(QWidget):
         super().__init__(parent)
 
         layout = QGridLayout()
+        layout.setRowMinimumHeight(1, 200)
+        layout.setRowMinimumHeight(2, 200)
+        layout.setColumnMinimumWidth(1, 400)
+        layout.setColumnMinimumWidth(2, 400)
+        layout.setColumnMinimumWidth(3, 400)
+
         layout.addWidget(RawData(self), 1, 1)
         layout.addWidget(Oncat(self), 2, 1)
         layout.addWidget(MDEType(self), 1, 2)
         layout.addWidget(ReductionParameters(self), 2, 2)
         layout.addWidget(Buttons(self), 1, 3, 2, 1)
+
         self.setLayout(layout)
-
-
-class RawData(QGroupBox):
-    """Raw data selection widget"""
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setTitle("Raw data")
 
 
 class Oncat(QGroupBox):
