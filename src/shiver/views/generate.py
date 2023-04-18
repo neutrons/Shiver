a1aa073042da85c044b5bef61af66a94d2921d7e
Chen Zhang <zhangc@ornl.gov>
2023-04-18 11:11:57 -0400

    add var and path checking

--- src/shiver/views/generate.py
+++ src/shiver/views/generate.py
@@ -1,4 +1,5 @@
 """PyQt widget for the histogram tab"""
+import re
 from qtpy.QtWidgets import (
     QWidget,
     QVBoxLayout,
@@ -31,9 +32,15 @@ class Generate(QWidget):
 
         layout.addWidget(RawData(self), 1, 1)
         layout.addWidget(Oncat(self), 2, 1)
-        layout.addWidget(MDEType(self), 1, 2)
+        self.mde_type_widget = MDEType(self)
+        layout.addWidget(self.mde_type_widget, 1, 2)
         layout.addWidget(ReductionParameters(self), 2, 2)
-        layout.addWidget(Buttons(self), 1, 3, 2, 1)
+
+        self.buttons = Buttons(self)
+        layout.addWidget(self.buttons, 1, 3, 2, 1)
+        # connect to as_dict for user to see the dict before the saving
+        # is fully implemented.
+        self.buttons.save_btn.clicked.connect(self.mde_type_widget.as_dict)
 
         self.setLayout(layout)
 
@@ -113,6 +120,60 @@ class MDEType(QGroupBox):
             self._output_dir = directory
             self.output_dir.setText(directory)
 
+    def check_mde_name(self) -> bool:
+        """Check if the mde name is valid.
+
+        Returns:
+        --------
+            bool: True if the mde name is valid, False otherwise.
+        """
+        mde_name = self.mde_name.text()
+
+        if is_valid_name(mde_name):
+            self._mde_name = mde_name
+            return True
+
+        return False
+
+    def check_output_dir(self) -> bool:
+        """Check if the output directory is valid.
+
+        Returns:
+        --------
+        bool:
+            True if the output directory is not empty and does not contains any
+            special char, False otherwise.
+        """
+        output_dir = self.output_dir.text()
+
+        if output_dir == "":
+            return False
+
+        if has_special_char(output_dir):
+            return False
+
+        self._output_dir = output_dir
+        return True
+
+    @property
+    def is_valid(self) -> bool:
+        """Check if the MDE type widget is valid."""
+        return self.check_mde_name() and self.check_output_dir()
+
+    def as_dict(self) -> dict:
+        """Return the MDE type widget as a dictionary."""
+        if self.is_valid:
+            rst = {
+                "mde_name": self._mde_name,
+                "output_dir": self._output_dir,
+                "mde_type": self.mde_type_button_group.checkedButton().text(),
+            }
+        else:
+            rst = {}
+
+        print(rst)
+        return rst
+
 
 class ReductionParameters(QGroupBox):
     """Generate reduction parameter widget"""
@@ -129,7 +190,56 @@ class Buttons(QWidget):
         super().__init__(parent)
 
         layout = QVBoxLayout()
-        layout.addWidget(QPushButton("Generate"))
-        layout.addWidget(QPushButton("Save settings"))
+        self.generate_btn = QPushButton("Generate")
+        layout.addWidget(self.generate_btn)
+        self.save_btn = QPushButton("Save settings")
+        layout.addWidget(self.save_btn)
         layout.addStretch()
         self.setLayout(layout)
+
+
+def is_valid_name(var_name: str) -> bool:
+    """Check if the variable name is valid.
+
+    Parameters:
+    -----------
+    var_name: str
+        The variable name to check.
+
+    Returns:
+    --------
+    bool:
+        True if the variable name is valid, False otherwise.
+
+    NOTE:
+    -----
+        The variable name must follow the following rules:
+        - must not be empty
+        - must start with a letter or underscore
+        - must only contain letters, numbers, and underscores
+    """
+    if not var_name:
+        return False
+    if not var_name[0].isalpha() and var_name[0] != "_":
+        return False
+    for char in var_name:
+        if not char.isalnum() and char != "_":
+            return False
+    return True
+
+
+def has_special_char(var_name: str) -> bool:
+    """Check if the variable name contains special characters.
+
+    Parameters:
+    -----------
+    var_name: str
+        The variable name to check.
+
+    Returns:
+    --------
+    bool:
+        True if the variable name contains special characters, False otherwise.
+    """
+    regex = re.compile("[@!#$%^&*()<>?|}{~]")
+    return regex.search(var_name) is not None
