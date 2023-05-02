890da5542c1fb6588b13ced371d7925e3b641f75
Ross Whitfield <whitfieldre@ornl.gov>
2023-05-02 12:16:52 -0400

    Update labels and add MDE table legend

--- src/shiver/views/histogram_parameters.py
+++ src/shiver/views/histogram_parameters.py
@@ -77,7 +77,7 @@ class HistogramParameter(QGroupBox):
     """Histogram parameters widget"""
 
     plot_num = 1
-    name_base = "Plot"
+    name_base = "Histogram"
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -531,7 +531,7 @@ class Dimensions(QWidget):
         layout.addWidget(QLabel("Dimensions"), 0, 0)
         layout.addWidget(QLabel("Minimum"), 0, 1)
         layout.addWidget(QLabel("Maximum"), 0, 2)
-        layout.addWidget(QLabel("Step"), 0, 3)
+        layout.addWidget(QLabel("Step Size"), 0, 3)
 
         self.positive_double_validator = QtGui.QDoubleValidator(self)
         self.positive_double_validator.setBottom(1e-10)
