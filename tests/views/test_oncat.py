8844810948e44c5f8e8fef418a2c717d320b5912
Chen Zhang <zhangc@ornl.gov>
2023-05-03 17:03:23 -0400

    add unit test for oncat agent

--- tests/views/test_oncat.py
+++ tests/views/test_oncat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
+# pylint: disable=all
 """Test the views for the ONCat application."""
 import pytest
-import pyoncat
 from shiver.views.oncat import (
     OncatToken,
     OnCatAgent,
@@ -13,11 +13,25 @@ from shiver.views.oncat import (
 )
 
 
+class MockRecord:
+    def __init__(self, *args, **kwargs) -> None:
+        pass
+
+    def list(self, *args, **kwargs) -> list:
+        return [{"id": "test_id"}, {"id": "test_id2"}]
+
+
 class MockONcat:
     """Mock Oncat instance for testing"""
 
     def __init__(self, *args, **kwargs) -> None:
+        self.Facility = MockRecord()
+        self.Experiment = MockRecord()
+
+    def login(self, *args, **kwargs) -> None:
+        """Mock login"""
         pass
+
 
 def test_oncat_token(tmp_path):
     """Test the OncatToken class."""
@@ -34,7 +48,28 @@ def test_oncat_token(tmp_path):
 
 def test_oncat_agent(monkeypatch):
     """Test the OnCatAgent class."""
-    pass
+
+    # mockey patch get_dataset_names
+    def mock_get_dataset_names(*args, **kwargs):
+        return ["test1", "test2"]
+
+    monkeypatch.setattr("shiver.views.oncat.get_dataset_names", mock_get_dataset_names)
+
+    # mockey patch class pyoncat.ONCat
+    monkeypatch.setattr("pyoncat.ONCat", MockONcat)
+
+    # test the class
+    agent = OnCatAgent()
+    # test login
+    agent.login("test_login", "test_password")
+    # test is_connected
+    assert agent.is_connected is True
+    # test get_ipts
+    assert agent.get_ipts("facility", "inst") == ["test_id", "test_id2"]
+    # test get_dataset_names
+    assert agent.get_datasets("facility", "inst", 1) == ["test1", "test2"]
+    # test get agent instance
+    assert agent.get_agent_instance() is not None
 
 
 def test_oncat_login(monkeypatch):
@@ -49,12 +84,13 @@ def test_oncat():
 
 def test_get_data_from_oncat():
     """Use mock to test get_data_from_oncat."""
+
     # make a mock pyoncat agent
     class MockDataFile:
         def __init__(self, name):
             self.name = name
 
-        def list(self, **kwargs):  # pylint: disable=unused-argument
+        def list(self, **kwargs):
             return ["a", "b", "c"]
 
     class MockAgent:
@@ -68,30 +104,18 @@ def test_get_data_from_oncat():
         agent,
         "projection",
         "ipts",
-        "inst",) == ["a", "b", "c"]
+        "inst",
+    ) == ["a", "b", "c"]
 
 
 def test_get_dataset_names(monkeypatch):
     """Use mock to test get_dataset_names."""
+
     # monkeypatch get_data_from_oncat
-    def mock_get_data_from_oncat(*args, **kwargs):  # pylint: disable=unused-argument
+    def mock_get_data_from_oncat(*args, **kwargs):
         mock_data = [
-            {"metadata": 
-                {
-                    "entry": {
-                        "notes": "a"
-                        }
-                }
-            },
-            {"metadata": 
-                {
-                    "entry": {
-                        "daslogs": {
-                             "sequencename": {"value": "b"}
-                            }
-                        }
-                }
-            },
+            {"metadata": {"entry": {"notes": "a"}}},
+            {"metadata": {"entry": {"daslogs": {"sequencename": {"value": "b"}}}}},
         ]
         return mock_data
 
@@ -104,35 +128,30 @@ def test_get_dataset_names(monkeypatch):
 
 def test_get_dataset_info(monkeypatch):
     """Use mock to test get_dataset_info."""
+
     # monkeypatch get_data_from_oncat
-    def mock_get_data_from_oncat(*args, **kwargs):  # pylint: disable=unused-argument
+    def mock_get_data_from_oncat(*args, **kwargs):
         mock_data = [
-            {   
+            {
                 "location": "/tmp/a",
                 "indexed": {
                     "run_number": 1,
                 },
-                "metadata": 
-                {
-                    "entry": {
-                        "notes": "a"
-                        }
-                }
+                "metadata": {"entry": {"notes": "a"}},
             },
             {
                 "location": "/tmp/b",
                 "indexed": {
                     "run_number": 2,
                 },
-                "metadata": 
-                {
+                "metadata": {
                     "entry": {
                         "daslogs": {
-                             "sequencename": {"value": "b"},
-                             "omega": {"average": 1.0},
-                            }
+                            "sequencename": {"value": "b"},
+                            "omega": {"average": 1.0},
                         }
-                }
+                    }
+                },
             },
         ]
         return mock_data
@@ -146,7 +165,7 @@ def test_get_dataset_info(monkeypatch):
         instrument="inst",
         dataset_name="a",
         use_notes=True,
-    ) == ['/tmp/a']
+    ) == ["/tmp/a"]
 
     # test the function (new format)
     assert get_dataset_info(
@@ -155,7 +174,7 @@ def test_get_dataset_info(monkeypatch):
         instrument="inst",
         dataset_name="b",
         use_notes=False,
-    ) == ['/tmp/b']
+    ) == ["/tmp/b"]
 
 
 if __name__ == "__main__":
