50aff7db77d5116d6ce09a687d9fb06c325690f1
Maria Patrou <patroum@ornl.gov>
2023-05-10 09:24:56 -0400

    pre-commit edits added

--- src/shiver/views/sample.py
+++ src/shiver/views/sample.py
@@ -450,7 +450,7 @@ class LatticeParametersWidget(QWidget):
         self.angle_validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
         self.double_validator = QtGui.QDoubleValidator(self)
         self.double_validator.setNotation(QtGui.QDoubleValidator.StandardNotation)
-        
+
         # 1 row: a, b, c
         grid.addWidget(QLabel("a"), 0, 0)
         self.latt_a = QLineEdit()
