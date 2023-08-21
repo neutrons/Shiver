25fdfea473a39e4583696e5acad3de1b02bacaec
Maria Patrou <patroum@ornl.gov>
2023-08-21 09:53:51 -0400

    plots tests added/updated for configuration settings

--- src/shiver/views/plots.py
+++ src/shiver/views/plots.py
@@ -2,29 +2,44 @@
 import matplotlib.pyplot as plt
 from mantidqt.widgets.sliceviewer.presenters.presenter import SliceViewer
 from mantidqt.plotting.functions import manage_workspace_names, plot_md_ws_from_names
+from shiver.configuration import get_data
 
 
 @manage_workspace_names
-def do_1d_plot(workspaces, display_name, intensity_limits=None):
+def do_1d_plot(workspaces, display_name, intensity_limits=None, log_scale=False):
     """Create an 1D plot for the provided workspace"""
     fig = plot_md_ws_from_names(workspaces, False, False)
     plot_title = display_name if display_name else workspaces[0].name()
     min_limit = intensity_limits["min"] if intensity_limits is not None and "min" in intensity_limits else None
     max_limit = intensity_limits["max"] if intensity_limits is not None and "max" in intensity_limits else None
-    fig.axes[0].set_ylim(min_limit, max_limit)
+
+    # logarithic case
+    if log_scale:
+        fig.axes[0].set_yscale("log")
+
+    # y limits
+    if not (min_limit is None and max_limit is None):
+        fig.axes[0].set_ylim(min_limit, max_limit)
+
     fig.canvas.manager.set_window_title(display_name)
     fig.axes[0].set_title(plot_title)
     return fig
 
 
 @manage_workspace_names
-def do_colorfill_plot(workspaces, display_name=None, intensity_limits=None):
+def do_colorfill_plot(workspaces, display_name=None, intensity_limits=None, log_scale=False):
     """Create a colormesh plot for the provided workspace"""
     fig, axis = plt.subplots(subplot_kw={"projection": "mantid"})
     plot_title = display_name or workspaces[0].name()
+    # y limits
     min_limit = intensity_limits["min"] if intensity_limits is not None and "min" in intensity_limits else None
     max_limit = intensity_limits["max"] if intensity_limits is not None and "max" in intensity_limits else None
-    colormesh = axis.pcolormesh(workspaces[0], vmin=min_limit, vmax=max_limit)
+
+    # logarithic case
+    scale_norm = "linear"
+    if log_scale:
+        scale_norm = "log"
+    colormesh = axis.pcolormesh(workspaces[0], vmin=min_limit, vmax=max_limit, norm=scale_norm)
     axis.set_title(plot_title)
     fig.canvas.manager.set_window_title(plot_title)
 
@@ -34,7 +49,7 @@ def do_colorfill_plot(workspaces, display_name=None, intensity_limits=None):
 
 
 @manage_workspace_names
-def do_slice_viewer(workspaces, parent=None, intensity_limits=None):
+def do_slice_viewer(workspaces, parent=None, intensity_limits=None, log_scale=False):
     """Open sliceviewer for the provided workspace"""
     presenter = SliceViewer(ws=workspaces[0], parent=parent)
 
@@ -48,10 +63,19 @@ def do_slice_viewer(workspaces, parent=None, intensity_limits=None):
         if intensity_limits is not None and intensity_limits["max"] is not None
         else presenter.view.data_view.colorbar.cmax_value
     )
-    presenter.view.data_view.colorbar.cmin.setText(f"{min_limit:.4}")
-    presenter.view.data_view.colorbar.clim_changed()
-    presenter.view.data_view.colorbar.cmax.setText(f"{max_limit:.4}")
-    presenter.view.data_view.colorbar.clim_changed()
+
+    # y limits
+    if not (min_limit is None and max_limit is None):
+        presenter.view.data_view.colorbar.cmin.setText(f"{min_limit:.4}")
+        presenter.view.data_view.colorbar.clim_changed()
+        presenter.view.data_view.colorbar.cmax.setText(f"{max_limit:.4}")
+        presenter.view.data_view.colorbar.clim_changed()
+
+    # logarithic case
+    if log_scale:
+        norm_scale = "Log"
+        presenter.view.data_view.colorbar.norm.setCurrentText(norm_scale)
+        presenter.view.data_view.colorbar.norm_changed()
 
     presenter.view.show()
     return presenter.view
@@ -59,10 +83,11 @@ def do_slice_viewer(workspaces, parent=None, intensity_limits=None):
 
 def do_default_plot(workspace, ndims, display_name=None, intensity_limits=None):
     """Create the default plot for the workspace and number of dimensions"""
+    log_scale = get_data("main_tab.plot", "logarithmic_intensity")
     if ndims == 1:
-        return do_1d_plot([workspace], display_name, intensity_limits)
+        return do_1d_plot([workspace], display_name, intensity_limits, log_scale)
     if ndims == 2:
-        return do_colorfill_plot([workspace], display_name, intensity_limits)
+        return do_colorfill_plot([workspace], display_name, intensity_limits, log_scale)
     if ndims in (3, 4):
-        return do_slice_viewer([workspace], intensity_limits=intensity_limits)
+        return do_slice_viewer([workspace], intensity_limits=intensity_limits, log_scale=log_scale)
     return None
--- tests/views/test_plots.py
+++ tests/views/test_plots.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 """UI tests for Plots"""
+import pytest
+
 # pylint: disable=no-name-in-module
 from mantid.simpleapi import (
     mtd,
     CreateMDHistoWorkspace,
 )
 from shiver.views.plots import do_default_plot
+from shiver.views.histogram import Histogram
 
 
-def test_plot1d(qtbot):
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot1d(qtbot, user_conf_file, monkeypatch):
     """Test for 1D plot with intensities and display title"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
 
     # clear mantid workspace
     mtd.clear()
@@ -30,11 +47,65 @@ def test_plot1d(qtbot):
     fig = do_default_plot(workspace, 1, title, {"min": intensity_min, "max": intensity_max})
     assert fig.axes[0].get_title() == title
     assert fig.axes[0].get_ylim() == (intensity_min, intensity_max)
-    qtbot.wait(500)
+    assert fig.axes[0].get_yscale() == "linear"
+
+    qtbot.wait(100)
 
 
-def test_plot2d(qtbot):
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = True
+    """
+    ],
+    indirect=True,
+)
+def test_plot1d_scale(qtbot, user_conf_file, monkeypatch):
+    """Test for 1D plot with log scale"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
+
+    # clear mantid workspace
+    mtd.clear()
+
+    workspace = CreateMDHistoWorkspace(
+        Dimensionality=1,
+        Extents="-3,3",
+        SignalInput=range(0, 10),
+        ErrorInput=range(0, 10),
+        NumberOfBins="10",
+        Names="Dim1",
+        Units="MomentumTransfer",
+    )
+
+    intensity_min = None
+    intensity_max = None
+    title = "1D Plot"
+    fig = do_default_plot(workspace, 1, title, {"min": intensity_min, "max": intensity_max})
+    assert fig.axes[0].get_yscale() == "log"
+    qtbot.wait(100)
+
+
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot2d(qtbot, user_conf_file, monkeypatch):
     """Test for 2D plot with intensities and display title"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
 
     # clear mantid workspace
     mtd.clear()
@@ -54,12 +125,69 @@ def test_plot2d(qtbot):
     title = "2D Plot"
     fig = do_default_plot(workspace, 2, title, {"min": intensity_min, "max": intensity_max})
     assert fig.axes[0].get_title() == title
-    assert fig.axes[0].collections[0].get_clim() == (intensity_min, intensity_max)
-    qtbot.wait(500)
+    assert fig.axes[1].collections[1].get_clim() == (intensity_min, intensity_max)
+    assert fig.axes[1].get_yscale() == "linear"
+
+    qtbot.wait(100)
 
 
-def test_plot3d(qtbot):
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = True
+    """
+    ],
+    indirect=True,
+)
+def test_plot2d_scale(qtbot, user_conf_file, monkeypatch):
+    """Test for 2D plot with log scale"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
+
+    # clear mantid workspace
+    mtd.clear()
+
+    workspace = CreateMDHistoWorkspace(
+        Dimensionality=2,
+        Extents="-2,2,-5,5",
+        SignalInput=range(0, 100),
+        ErrorInput=range(0, 100),
+        NumberOfBins="10,10",
+        Names="Dim1,Dim2",
+        Units="Momentum,Energy",
+    )
+
+    intensity_min = 0.001
+    intensity_max = 1
+    title = "2D Plot"
+    fig = do_default_plot(workspace, 2, title, {"min": intensity_min, "max": intensity_max})
+    assert fig.axes[0].get_title() == title
+    assert fig.axes[1].collections[1].get_clim() == (intensity_min, intensity_max)
+    assert fig.axes[1].get_yscale() == "log"
+
+    qtbot.wait(100)
+
+
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot3d(qtbot, user_conf_file, monkeypatch):
     """Test for 3D plot with intensities"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
 
     # clear mantid workspace
     mtd.clear()
@@ -80,12 +208,63 @@ def test_plot3d(qtbot):
     view = do_default_plot(workspace, 3, title, {"min": intensity_min, "max": intensity_max})
     assert view.data_view.colorbar.cmin_value == intensity_min
     assert view.data_view.colorbar.cmax_value == intensity_max
+    assert view.data_view.colorbar.norm.currentText() == "Linear"
 
-    qtbot.wait(500)
+    qtbot.wait(100)
 
 
-def test_plot4d(qtbot):
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = True
+    """
+    ],
+    indirect=True,
+)
+def test_plot3d_scale(qtbot, user_conf_file, monkeypatch):
+    """Test for 3D plot with log scale"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
+
+    # clear mantid workspace
+    mtd.clear()
+
+    workspace = CreateMDHistoWorkspace(
+        Dimensionality=3,
+        Extents="-5,5,-10,10,-20,20",
+        SignalInput=range(0, 1000),
+        ErrorInput=range(0, 1000),
+        NumberOfBins="10,10,10",
+        Names="Dim1,Dim2,Dim3",
+        Units="Energy,Momentum,Other",
+    )
+
+    title = None
+    view = do_default_plot(workspace, 3, title)
+    assert view.data_view.colorbar.norm.currentText() == "Log"
+    qtbot.wait(100)
+
+
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot4d(qtbot, user_conf_file, monkeypatch):
     """Test for 4D plot with intensities"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
 
     # clear mantid workspace
     mtd.clear()
@@ -106,12 +285,69 @@ def test_plot4d(qtbot):
     view = do_default_plot(workspace, 4, title, {"min": intensity_min, "max": intensity_max})
     assert view.data_view.colorbar.cmin_value == intensity_min
     assert view.data_view.colorbar.cmax_value == intensity_max
+    assert view.data_view.colorbar.norm.currentText() == "Linear"
+
+    qtbot.wait(100)
+
+
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = True
+    """
+    ],
+    indirect=True,
+)
+def test_plot4d_invalid_scale(qtbot, user_conf_file, monkeypatch):
+    """Test for 4D plot with intensities"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
+
+    # clear mantid workspace
+    mtd.clear()
+
+    workspace = CreateMDHistoWorkspace(
+        Dimensionality=4,
+        Extents="-5,5,-10,10,-20,20,-30,30",
+        SignalInput=range(0, 10000),
+        ErrorInput=range(0, 10000),
+        NumberOfBins="10,10,10,10",
+        Names="Dim1,Dim2,Dim3,Dim4",
+        Units="M,E,O,EX",
+    )
+
+    intensity_min = -10.34
+    intensity_max = 12.09
+    title = None
+    view = do_default_plot(workspace, 4, title, {"min": intensity_min, "max": intensity_max})
+    # mantid plot updates user values if invalid are passed
+    assert view.data_view.colorbar.cmin_value == 0.0001
+    assert view.data_view.colorbar.cmax_value == intensity_max
+    assert view.data_view.colorbar.norm.currentText() == "Log"
 
     qtbot.wait(500)
 
 
-def test_plot5d(qtbot):
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot5d(qtbot, user_conf_file, monkeypatch):
     """Test for 5D plot with intensities -invalid"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
 
     # clear mantid workspace
     mtd.clear()
@@ -131,4 +367,134 @@ def test_plot5d(qtbot):
     title = None
     view = do_default_plot(workspace, 5, title, {"min": intensity_min, "max": intensity_max})
     assert view is None
+    qtbot.wait(100)
+
+
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = name_only
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot_data_name_only(qtbot, user_conf_file, monkeypatch):
+    """Test plot inputs"""
+
+    # mock get_oncat_url, client_id and use_notes info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
+    histogram = Histogram()
+    # clear mantid workspace
+    mtd.clear()
+
+    workspace = CreateMDHistoWorkspace(
+        Dimensionality=4,
+        Extents="-3,3,-10,10,-20,20,-30,30",
+        SignalInput=range(0, 10000),
+        ErrorInput=range(0, 10000),
+        NumberOfBins="10,10,10,10",
+        Names="Dim1,Dim2,Dim3,Dim4",
+        Units="EnergyT,MomentumT,Other,Extra",
+    )
+
+    data = histogram.get_plot_data(workspace, 4)
+    assert data[0] == "workspace"
+    assert data[1]["min"] is None
+    assert data[1]["max"] is None
+
+    qtbot.wait(100)
+
+
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = full
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot_data_full(qtbot, user_conf_file, monkeypatch):
+    """Test plot inputs"""
+
+    # mock plot info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
+    histogram = Histogram()
+
+    def monk_display_name(ws_name, ndims):
+        return f"full {ws_name}: {ndims}"
+
+    histogram.plot_display_name_callback = monk_display_name
+
+    # clear mantid workspace
+    mtd.clear()
+
+    workspace = CreateMDHistoWorkspace(
+        Dimensionality=4,
+        Extents="-3,3,-10,10,-20,20,-30,30",
+        SignalInput=range(0, 10000),
+        ErrorInput=range(0, 10000),
+        NumberOfBins="10,10,10,10",
+        Names="Dim1,Dim2,Dim3,Dim4",
+        Units="EnergyT,MomentumT,Other,Extra",
+    )
+
+    intensity_min = -10.34
+    intensity_max = 12.09
+    histogram.histogram_parameters.dimensions.intensity_min.setText(str(intensity_min))
+    histogram.histogram_parameters.dimensions.intensity_max.setText(str(intensity_max))
+
+    data = histogram.get_plot_data(workspace, 4)
+    assert data[0] == "full workspace: 4"
+    assert data[1]["min"] == intensity_min
+    assert data[1]["max"] == intensity_max
+
+    qtbot.wait(100)
+
+
+@pytest.mark.parametrize(
+    "user_conf_file",
+    [
+        """
+        [main_tab.plot]
+        title = None
+        logarithmic_intensity = False
+    """
+    ],
+    indirect=True,
+)
+def test_plot_data_none(qtbot, user_conf_file, monkeypatch):
+    """Test plot inputs"""
+
+    # mock plot info
+    monkeypatch.setattr("shiver.configuration.CONFIG_PATH_FILE", user_conf_file)
+    histogram = Histogram()
+
+    # clear mantid workspace
+    mtd.clear()
+
+    workspace = CreateMDHistoWorkspace(
+        Dimensionality=4,
+        Extents="-3,3,-10,10,-20,20,-30,30",
+        SignalInput=range(0, 10000),
+        ErrorInput=range(0, 10000),
+        NumberOfBins="10,10,10,10",
+        Names="Dim1,Dim2,Dim3,Dim4",
+        Units="EnergyT,MomentumT,Other,Extra",
+    )
+
+    intensity_max = 12.09
+
+    histogram.histogram_parameters.dimensions.intensity_max.setText(str(intensity_max))
+
+    data = histogram.get_plot_data(workspace, 4)
+    assert data[0] is None
+    assert data[1]["min"] is None
+    assert data[1]["max"] == intensity_max
+
     qtbot.wait(100)
