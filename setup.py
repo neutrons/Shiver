8dc75ba547f205fea5bfafadb4d688c714695c46
Maria Patrou <patroum@ornl.gov>
2023-08-28 11:55:50 -0400

    setup added in pre-commit hook

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
-# this file is necessary so conda can read the contents of setup.cfg using
-# its load_setup_py_data function
+"""
+This file is necessary so conda can read the contents of setup.cfg using
+its load_setup_py_data function
+"""
 from setuptools import setup
 from versioningit import get_cmdclasses
 
