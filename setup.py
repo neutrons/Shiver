b58fca74c48b141b0f1301baeeb0ecb630b3731c
Maria Patrou <patroum@ornl.gov>
2023-08-30 20:00:11 -0400

    dependencies updated, python removed,setup.py removed

--- setup.py
+++
@@ -1,8 +0,0 @@
-"""
-This file is necessary so conda can read the contents of setup.cfg using
-its load_setup_py_data function
-"""
-from setuptools import setup
-from versioningit import get_cmdclasses
-
-setup(cmdclass=get_cmdclasses())
