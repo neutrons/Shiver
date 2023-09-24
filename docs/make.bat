f1e05b9c570520fc98e34b8389b1b4f80c058879
Maria Patrou <patroum@ornl.gov>
2023-09-24 17:44:38 -0400

    add  xvfb-run for sphinx build

--- docs/make.bat
+++ docs/make.bat
@@ -27,7 +27,7 @@ if errorlevel 9009 (
 
 if "%1" == "" goto help
 
-%SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
+xvfb-run %SPHINXBUILD% -M %1 %SOURCEDIR% %BUILDDIR% %SPHINXOPTS% %O%
 goto end
 
 :help
