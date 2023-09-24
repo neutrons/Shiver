13f26aae02c926bdc5e748a64a5883e181eca364
Maria Patrou <patroum@ornl.gov>
2023-09-24 17:08:37 -0400

    mock qt in conf file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,9 @@ extensions = [
     "sphinx.ext.intersphinx",
 ]
 
+autodoc_mock_imports = ["PyQt", "PyQt.QtGui", "PyQt.QtCore", "PyQt.QtWidgets"]
+
+
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
     "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
