9436d03ed36cee3c39e733b6e92b276d07008055
Maria Patrou <patroum@ornl.gov>
2023-11-20 16:55:55 -0500

    qtbot wait until for mainwindow test

--- tests/views/test_mainwindow.py
+++ tests/views/test_mainwindow.py
@@ -2,10 +2,10 @@
 from shiver import Shiver, __version__
 
 
-def test_mainwindow():
+def test_mainwindow(qtbot):
     """Test that the application starts successfully"""
     shiver = Shiver()
     shiver.show()
-
+    qtbot.waitUntil(shiver.show, timeout=5000)
     assert shiver.isVisible()
     assert shiver.windowTitle() == f"SHIVER - {__version__}"
