94080f9bf68c8098229504fab0ba72147d5f3c64
Kyle Ma <kyleqianlima@gmail.com>
2025-03-13 18:57:54 -0400

    test errors

--- tests/models/test_corrections.py
+++ tests/models/test_corrections.py
@@ -3,6 +3,7 @@
 # pylint: disable=invalid-name
 # pylint: disable=no-name-in-module
 from mantid.simpleapi import CreateMDWorkspace, FakeMDEventData
+import pytest
 from shiver.models import corrections
 
 
@@ -24,3 +25,22 @@ def test_applied_magnetic_form_factor():
     model.apply_magnetic_form_factor_correction("ws", "Nd3", "test")
     assert model.has_magnetic_form_factor_correction("test")[0]
     assert model.has_magnetic_form_factor_correction("test")[1] == "Nd3"
+    with pytest.raises(ValueError):
+        model.apply_magnetic_form_factor_correction("ws", "wrongIon", "test")
+    with pytest.raises(KeyError):
+        model.apply_magnetic_form_factor_correction_finished(
+            "ws", alg=corrections.MagneticFormFactorCorrectionMDObserver(model, "ws")
+        )
+
+
+def test_applied_debye_waller_factor_error():
+    """test debye-waller is applied"""
+    ws = CreateMDWorkspace(Dimensions="1", Extents="1,4", Names="|Q|", Units="A")
+    FakeMDEventData(ws, UniformParams=-6000)
+    model = corrections.CorrectionsModel()
+    with pytest.raises(ValueError):
+        model.apply_debye_waller_factor_correction("ws", "-3", "test")
+    with pytest.raises(KeyError):
+        model.apply_debye_waller_factor_correction_finished(
+            "ws", alg=corrections.DebyeWallerFactorCorrectionMDObserver(model, "ws")
+        )
