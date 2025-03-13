13eea0dd564d822ffd4d370ddbf67ffc537fb946
Kyle Ma <kyleqianlima@gmail.com>
2025-03-13 16:18:33 -0400

    more pylints

--- tests/models/test_corrections.py
+++ tests/models/test_corrections.py
@@ -1,22 +1,27 @@
 """Test the histogram workspace saving"""
 
-# pylint: disable=too-many-lines
-# Need to import the new algorithms so they are registered with mantid
-import shiver.models.makeslice  # noqa: F401, E402 pylint: disable=unused-import, wrong-import-order
-from mantid.api import PythonAlgorithm, AlgorithmFactory, MatrixWorkspaceProperty, WorkspaceFactory
-from mantid.kernel import Direction
-import shiver.models.corrections as corrections
+# pylint: disable=invalid-name
+from mantid.api import (  # pylint: disable=no-name-in-module
+    PythonAlgorithm,
+    AlgorithmFactory,
+    MatrixWorkspaceProperty,
+    WorkspaceFactory,
+)
+from mantid.kernel import Direction  # pylint: disable=no-name-in-module
+from shiver.models import corrections
 
 
 class DebyeWallerFactorCorrectionMD(PythonAlgorithm):
     """create a dummy function to test if the algorithms has been applied"""
 
     def PyInit(self):
+        """init"""
         self.declareProperty("InputWorkspace", "")
         self.declareProperty("MeanSquaredDisplacement", "")
         self.declareProperty(MatrixWorkspaceProperty("OutputWorkspace", "", direction=Direction.Output))
 
     def PyExec(self):
+        """execute"""
         endrange = int(self.getProperty("MeanSquaredDisplacement").value)  # Convert to int
         wspace = WorkspaceFactory.create("Workspace2D", NVectors=1, XLength=endrange, YLength=endrange)
         self.setProperty("OutputWorkspace", wspace)  # Stores the workspace as the given name
@@ -26,16 +31,19 @@ class MagneticFormFactorCorrectionMD(PythonAlgorithm):
     """create a dummy function to test if the algorithms has been applied"""
 
     def PyInit(self):
+        """init"""
         self.declareProperty("InputWorkspace", "")
         self.declareProperty("IonName", "")
         self.declareProperty(MatrixWorkspaceProperty("OutputWorkspace", "", direction=Direction.Output))
 
     def PyExec(self):
+        """execute"""
         wspace = WorkspaceFactory.create("Workspace2D", NVectors=1, XLength=3, YLength=3)
         self.setProperty("OutputWorkspace", wspace)  # Stores the workspace as the given name
 
 
 def test_applied_debye_waller_factor():
+    """test debye-waller is applied"""
     AlgorithmFactory.subscribe(DebyeWallerFactorCorrectionMD)
     model = corrections.CorrectionsModel()
     model.apply_debye_waller_factor_correction("doesnotmatter", "3", "test")
@@ -44,6 +52,7 @@ def test_applied_debye_waller_factor():
 
 
 def test_applied_magnetic_form_factor():
+    """test magnetic form factor is applied"""
     AlgorithmFactory.subscribe(MagneticFormFactorCorrectionMD)
     model = corrections.CorrectionsModel()
     model.apply_magnetic_form_factor_correction("doesnotmatter", "Nd3+", "test")
